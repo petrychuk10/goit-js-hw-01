137c72e675923d68409a561a8d6944d1773c830d
petrychuk10 <petrychuk10@gmail.com>
2020-06-16 09:36:52 +0300

    fix2-3

--- task-2.js
+++ task-2.js
@@ -2,11 +2,12 @@ const total = 100;
 const ordered = 50;
 
 const order = prompt("Введите желаемое количество товаров:");
+if (Number(prompt) !== NaN) {
+  if (Number(order) <= total) {
+    alert("Заказ оформлен, с Вами свяжется менеджер");
+  }
 
-if (Number(order) <= total) {
-  alert("Заказ оформлен, с Вами свяжется менеджер");
-}
-
-if (Number(order) > total) {
-  alert("На складе недостаточно товаров!");
+  if (Number(order) > total) {
+    alert("На складе недостаточно товаров!");
+  }
 }
--- task-3.js
+++ task-3.js
@@ -3,14 +3,10 @@ let message;
 
 const password = prompt("Введите пароль:");
 
-if (password === ADMIN_PASSWORD) {
-  message = "Добро пожаловать!";
-} else {
-  message = "Доступ запрещен, неверный пароль!";
-}
-
-if (password === null) {
-  message = "Отменено пользователем!";
-}
+password
+  ? password === ADMIN_PASSWORD
+    ? (message = "Добро пожаловать!")
+    : (message = "Доступ запрещен, неверный пароль!")
+  : (message = "Отменено пользователем!");
 
 alert(message);
